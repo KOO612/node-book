88cc404f86720d3154c4c36083f3a3a22b0cd8c9
KOO612 <ksm99007@gmail.com>
2023-12-26 17:12:01 +0900

    6-5강

--- app.js
+++ app.js
@@ -14,15 +14,20 @@ app.use('/', express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
+// 최신버전은 상관 없지만 cookie 뒤에 session 오도록 권장
 app.use(
   session({
+    // 요청이 올 때 세션에 수정 사항이 생기지 않더라도 다시 저장
     resave: false,
+    // 세션 저장할 내역이 없더라도 처음부터 세션을 생성할지
     saveUninitialized: false,
+    // 쿠키 서명
     secret: process.env.COOKIE_SECRET,
     cookie: {
       httpOnly: true,
       secure: false,
     },
+    // 세션 쿠키의 이름
     name: 'session-cookie',
   })
 );
@@ -43,9 +48,20 @@ app.get(
   }
 );
 
+app.get('/cate/:name', (req, res) => {
+  res.send(`hello ${req.params.name}`);
+});
+
+// 404 처리 미들웨어
+// 틀린 경로 요청 시 404 페이지
+app.use((req, res, next) => {
+  res.send('404에러');
+});
+
 app.use((err, req, res, next) => {
   console.error(err);
-  res.status(500).send(err.message);
+  // 브라우저에 status 에러 코드를 보내 속임
+  res.status(200).send(err.message);
 });
 
 app.listen(app.get('port'), () => {
